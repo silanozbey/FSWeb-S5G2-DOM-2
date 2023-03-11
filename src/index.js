a58ec17378e75f9d67e3f2590cd3f7bcfd33abbb
Şilan Özbey <silanozbey02@gmail.com>
2023-03-12 00:00:07 +0300

    tamamlandı

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import './less/index.less'
 
-// Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
+function random(){
+    return Math.floor(Math.random()) * 256;
+}
 
+document.querySelector("h1").addEventListener("click",(e)=>{
+    alert("Otobüs Kalkıyoor!");    
+});
 
-// Kodlar buraya gelecek!
+let img=document.querySelectorAll("img")
+img.forEach((image) => {
+    image.addEventListener("dblclick", (e) => {
+    image.style.filter = "grayscale(100%)"; 
+    })
+})
+
+img.forEach((image) => {
+    image.addEventListener("click", (e) => {
+    image.style.filter = "unset"; 
+    })
+})
+
+let baslik=document.querySelector("h1");
+baslik.addEventListener("mouseover", (e)=>{
+    e.target.style.color="#ffda46";
+});
+
+baslik.addEventListener("mouseout", (e)=>{
+    e.target.style.color="#17a2b8";
+});
+
+document.querySelector(".intro").addEventListener("focus" , (e) => {
+    e.target.style.backgroundColor = "red";
+});
+
